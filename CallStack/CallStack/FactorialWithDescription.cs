f6936167666ff960e3a52eaa7a88d3def49cc8e5
MOSCOW\Sergey_Nikolskiy <Sergey_Noikolskiy@epam.com>
2019-04-29 11:44:32 +0300

    extract factorial function

---
+++ CallStack/CallStack/FactorialWithDescription.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallStack
+{
+	public class FactorialWithDescription
+	{
+		public FactorialWithDescription()
+		{
+			Step = 1;
+		}
+
+		public StringBuilder Description { get; } = new StringBuilder();
+
+		public int Step { get; private set; }
+				
+		public int RecurentFind(int source)
+		{
+			if (Step == 1)
+			{
+				Description.AppendLine($@"
+=========================================================
+We found factorial for {source} and show 
+how calls of functions stack in memory.
+Each function in call stack will be represetn by: -symbol
+=========================================================
+");
+			}
+
+			if (source != 1)
+			{
+				Description.AppendLine($"{MakeLeftPadding()} Recurent case step: {Step} source: {source}");
+
+				Step++;
+				int result = source * RecurentFind(source - 1);
+				Step--;
+
+				Description.AppendLine($"{MakeLeftPadding()} Back to step: {Step} result: {result}");
+
+				if (Step == 1)
+				{
+					Description.AppendLine($@"
+=========================================================
+Factorial is: {result}
+=========================================================
+");
+				}
+
+				return result;
+			}
+			else
+			{
+				Description.AppendLine($"{MakeLeftPadding()} BASE CASE: recursion End, now function run back on call stack");
+				return source;
+			}
+		}
+		
+
+		private string MakeLeftPadding()
+		{
+			StringBuilder result = new StringBuilder();
+
+			for (int i = 0; i < Step; i++)
+			{
+				result.Append("-");
+			}
+
+			return result.ToString();
+		}
+	}
+}
--- CallStack/CallStack/Program.cs
+++ CallStack/CallStack/Program.cs
@@ -18,53 +18,13 @@ namespace CallStack
 				Console.Write($"Entered data is not a number or value bigger then: {_max}, try again: ");
 				strNumber = Console.ReadLine();
 			}
+						
+			var factorial = new FactorialWithDescription();
+			int result = factorial.RecurentFind(number);
+			Console.WriteLine(factorial.Description);
 
-			Console.WriteLine("=================================");
-			Console.WriteLine($@"Now we found factorial of {number} 
-and show how calls of functions stack in memory.
-Each function in call stack will be represetn by: - symbol ");
-			Console.WriteLine("=================================");
-
-			int step = 1;
-			int result = RecurentFactorialWitDescription(number, step);
-
-			Console.WriteLine("=================================");
-			Console.WriteLine($"Factorial for: {number} is: {result}");
-			Console.WriteLine("=================================");
 			Console.WriteLine("Press any key to exit");
 			Console.ReadLine();
-		}
-
-		private static int RecurentFactorialWitDescription(int source, int step)
-		{
-			if (source != 1)
-			{
-				MakeLeftPadding(step);
-				Console.WriteLine($"Recurent case step: {step} source: {source}");
-				
-				step++;
-				int result = source * RecurentFactorialWitDescription(source - 1, step);
-				step--;
-				
-				MakeLeftPadding(step);
-				Console.WriteLine($"Back to step: {step} result: {result}");
-				return result;
-			}
-			else
-			{
-				MakeLeftPadding(step);
-
-				Console.WriteLine("BASE CASE: recursion End, now function run back on call stack");
-				return source;
-			}
-		}
-
-		private static void MakeLeftPadding(int count)
-		{
-			for (int i = 0; i < count; i++)
-			{
-				Console.Write("-");
-			}
 		}
 	}
 }
