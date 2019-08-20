44ca847d79945c77624026b186fd1bb201544a8d
Сергей Никольский <Sergey_Nikolskiy@epam.com>
2019-08-20 16:54:13 +0300

    add insertion sort

--- Sort/SortRepresentation/SelectionSort/SelectionSortWithDescription.cs
+++ Sort/SortRepresentation/SelectionSort/SelectionSortWithDescription.cs
@@ -1,38 +1,37 @@
-﻿using System.Text;
-
-namespace Sort
+﻿namespace Sort
 {
 	public class SelectionSortWithDescription : ISort
 	{
+		private string _sortRuleName;
+
 		public string Title => "=== Selection Sort Representation ===";
 
-		public string Description { get; private set; }
-
-		public void Descending(int[] source)
-		{
-			Sort(source, false);
-		}
-
-		public void Ascending(int[] source)
-		{
-			Sort(source, true);
-		}
-
-		private void Sort(int[] source, bool isAscending)
-		{
-			string sortRuleName = isAscending ? "ASCENDING" : "DESCENDING";
-
-			Description = $@"
+		public string Description => $@"
 =======================================
-|   {sortRuleName,10} sort with description  |
+|   {_sortRuleName,10} sort with description  |
 =============================================================
 | 1. Find value which most applicable to sorting direction. |
 | 2. Change this value with first position value in array.  |
 | 3. Repeat for not sorted part of array.                   |
 =============================================================
-| Complexity is: O(n^2)               |
-=======================================
+| Complexity is: O(n^2) |
+=========================
 ";
+
+		public void Descending(int[] source)
+		{
+			_sortRuleName = "DESCENDING";
+			Sort(source, false);
+		}
+
+		public void Ascending(int[] source)
+		{
+			_sortRuleName = "ASCENDING";
+			Sort(source, true);
+		}
+
+		private void Sort(int[] source, bool isAscending)
+		{
 			for (int i = 0; i < source.Length; i++)
 			{
 				for (int j = i + 1; j < source.Length; j++)
