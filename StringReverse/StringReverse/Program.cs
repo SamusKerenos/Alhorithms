d5514dadd672fe0fdd6a49ddf86b6280cef5617c
Сергей Никольский <Sergey_Nikolskiy@epam.com>
2019-09-09 15:43:26 +0300

    add string reverse alhorithm

---
+++ StringReverse/StringReverse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringReverse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("=== String revers alhorithm ===");
+
+			Console.WriteLine("Enter the some sting to revers it:");
+			string source = Console.ReadLine();
+
+			Console.WriteLine($"The result is: {source.Revert()}");
+		 }
+	}
+}
---
+++ StringReverse/StringReverse/StringReverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace StringReverse
+{
+	public static class StringReverter
+	{
+		public static string Revert(this string source)
+		{
+			char[] data = source.Select(c => c).ToArray();
+
+			for (int i = 0; i < (data.Length) / 2; i++)
+			{
+				int destinationIndex = (source.Length - 1) - i;
+
+				char mem = data[destinationIndex];
+				data[destinationIndex] = data[i];
+				data[i] = mem;
+			}
+
+			return new string(data);
+		}
+	}
+}
