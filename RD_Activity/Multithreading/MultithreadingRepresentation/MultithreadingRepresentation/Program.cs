d963744c84dba3ed128091a079f2f3791ab24328
Сергей Никольский <Sergey_Nikolskiy@epam.com>
2019-09-10 10:59:52 +0300

    add one thread representation

---
+++ RD_Activity/Multithreading/MultithreadingRepresentation/MultithreadingRepresentation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace MultithreadingRepresentation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("=== Simple representation of multithreading ===");
+			var worker = new Worker();
+			worker.ProgressChanged += OnProgressChanged;
+			worker.WorkComplete += OnWorkComplete;
+
+			Thread sideThred = new Thread(worker.Work);
+			sideThred.Start(50 as object);
+
+			if (StopWork())
+			{
+				worker.Cancel();
+			}
+		}
+
+		static void OnWorkComplete(bool canceled, int counter)
+		{
+			if (canceled)
+			{
+				Console.WriteLine($" work canceled at {counter} count");
+			}
+			else
+			{
+				Console.WriteLine($" work complete");
+			}
+		}
+
+		static void OnProgressChanged(string partialResult)
+		{
+			Console.Write(partialResult);
+		}
+
+		static bool StopWork()
+		{
+			Console.WriteLine("To stop work enter 'S' ");
+			string stop = string.Empty;
+			while (stop != "S")
+			{
+				stop = Console.ReadLine();
+			}
+			return true;
+		}
+	}
+}
---
+++ RD_Activity/Multithreading/MultithreadingRepresentation/MultithreadingRepresentation/Worker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultithreadingRepresentation
+{
+	public class Worker
+	{
+		private bool _canceled = false;
+
+		public void Cancel()
+		{
+			_canceled = true;
+		}
+
+		public void Work(object maxCounterObject)
+		{
+			if (maxCounterObject != null && maxCounterObject is int)
+			{
+				int counter = 1;
+				int maxCounter = (int)maxCounterObject;
+
+				while (!_canceled && counter < maxCounter)
+				{
+					Thread.Sleep(1000);
+					ProgressChanged("-work|");
+					counter++;
+				}
+
+				WorkComplete(_canceled, counter);
+			}
+		}
+
+		public event Action<string> ProgressChanged;
+
+		public event Action<bool, int> WorkComplete;
+	}
+}
